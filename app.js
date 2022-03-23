e305004f6913187782a49e2ec024a63513780f28
Jeron <norejudeisa@gmail.com>
2022-03-23 00:10:03 +0000

    changed project to a recipe app

--- app.js
+++ app.js
@@ -1,39 +1,116 @@
-const url =
-  'https://newsapi.org/v2/everything?q=Ghana&apiKey=a279f44e37f0418d942d310af34c9f93&pageSize=100'
-const newsContainer = document.querySelector('main')
+const generateRandomCharacter = () => {
+  const characters = 'abcdefghijklmnopqrstuvwz'
+  return characters.charAt(Math.floor(Math.random() * characters.length))
+}
+const char = generateRandomCharacter()
+const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${generateRandomCharacter()}`
+
 const logo = document.querySelector('.logo')
 const logoText = document.querySelector('.title')
-const spinner = document.querySelector('.spinner')
+const loader = document.querySelector('.loader')
+
+const main = document.querySelector('main')
+const modalOverlay = document.querySelector('.modal-overlay')
+
 logo.addEventListener('click', () => {
   window.scrollTo(0, 0)
 })
 logoText.addEventListener('click', () => {
   window.scrollTo(0, 0)
 })
-const getNews = async () => {
-  spinner.style.display = 'block'
-  const response = await fetch(url)
-  const { articles } = await response.json()
-  articles.forEach((article) => {
-    console.log(article)
-    const newsCard = document.createElement('div')
-    newsCard.classList.add('card')
-    newsCard.innerHTML = `
-    <div class="image-container">
-    <img src="${article.urlToImage}" alt="news image" />
-  </div>
-  <div class="content">
-    <p>
-     ${article.title}
-    </p>
-    <p class="desc">
-    ${article.description?.slice(0, 100)}...
-    </p>
-    <a href="${article.url}" target="_blank">Read More</a>
-  </div>
-    `
-    newsContainer.append(newsCard)
-  })
-  spinner.style.display = 'none'
+
+async function getRecipe(searchTerm = url) {
+  try {
+    loader.style.display = 'grid'
+    const response = await fetch(searchTerm)
+    const { meals } = await response.json()
+    main.innerHTML = ''
+    meals.forEach((item) => {
+      const iterator = Object.entries(item)
+      const listItems = iterator
+        .map((item) => {
+          const [key, value] = item
+          if (key.includes('Ingredient') && value !== null && value !== '') {
+            return `<li>${value}</li>`
+          }
+        })
+        .join('')
+
+      const card = document.createElement('div')
+      card.classList.add('card')
+      card.innerHTML = `
+      <div class="image-container">
+        <img src="${item.strMealThumb}" alt="fruit" />
+      </div>
+      <p class="meal-teaser">${item.strTags ? item.strTags : ''}</p>
+      <div class="meal-content">
+        <h2 class="meal-title">${item.strMeal}</h2>
+        <a href='${
+          item.strYoutube
+        }' class="recipe" target='_blank'>Learn to cook</a>
+      </div>
+   `
+      main.appendChild(card)
+      card.addEventListener('click', () => {
+        modalOverlay.innerHTML = `
+       <div class="modal">
+        <div class="header">
+          <button class="close">
+            <ion-icon name="close-outline"></ion-icon>
+          </button>
+          <h3>Apple Frangipan Tart</h3>
+          <p>${item.strCategory}</p>
+        </div>
+        <div class="ingredient">
+          <h4>Ingredients</h4>
+          <ul>
+          ${listItems}
+          </ul>
+        </div>
+        <div class="instructions">
+          <h4>Instructions</h4>
+          <p>
+            ${item.strInstructions}
+          </p>
+        </div>
+      </div>
+       `
+        modalOverlay.classList.add('open')
+        const closeBtn = document.querySelectorAll('.close')
+        closeBtn.forEach((btn) => {
+          btn.addEventListener('click', () => {
+            modalOverlay.classList.remove('open')
+          })
+        })
+      })
+    })
+    loader.style.display = 'none'
+  } catch (error) {
+    loader.style.display = 'none'
+    console.log(error.response)
+    Toastify({
+      text: 'There was an error finding your meal',
+      duration: 3000,
+      destination: 'https://github.com/apvarun/toastify-js',
+      newWindow: true,
+      close: true,
+      gravity: 'bottom', // `top` or `bottom`
+      position: 'center', // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: 'linear-gradient(to right, #3454e2, #da18d0)',
+      },
+    }).showToast()
+  }
 }
-getNews()
+getRecipe()
+// FORM
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const text = form.children[0]
+  const value = `https://www.themealdb.com/api/json/v1/1/search.php?s=${text.value.trim()}`
+  getRecipe(value)
+  text.value = ''
+})
